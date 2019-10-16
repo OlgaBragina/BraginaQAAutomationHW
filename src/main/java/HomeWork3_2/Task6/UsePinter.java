671c239e88765931806f7ff2c13a44ff6de0bfb1
olgabragina <olha.b@allsetnow.com>
2019-10-16 22:00:44 +0300

    added printer

--- src/main/java/HomeWork3_2/Task6/UsePinter.java
+++ src/main/java/HomeWork3_2/Task6/UsePinter.java
@@ -1,4 +1,12 @@
 package HomeWork3_2.Task6;
 
 public class UsePinter {
+    static Printer sumsung = new Printer( "sumsung", "XR3",true,
+            "Laser", "Yes", 100, 100,
+            100, 100, 13);
+
+    public static void main(String[] args) {
+        sumsung.fillUp();
+        sumsung.printPage(13);
+    }
 }
